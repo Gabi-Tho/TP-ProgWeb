7ad34eed2edb0331a38bf04ec97eb8635536169c
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2022-05-06 18:03:48 -0400

    start year code section

--- Ajout-clients.php
+++ Ajout-clients.php
@@ -9,19 +9,69 @@ $prenom     = $_POST["prenom"] ?? "";
 $naissance     = $_POST["naissance"] ?? "";
 $telephone     = $_POST["telephone"] ?? "";
 $messageAjout = "Ajout effectué dans la base de données";
-$errorNom = "au moins deux characteres";
-$errorPrenom = "au moins deux characteres";
+$errorChar = "au moins un characteres spéciale";
+$errorLength = "au moins deux characteres";
 $errorNaissance = "doit etre majeur";
-$errorTelephone = "format incorrecte. Doit etre 123-123-1234";
+$errorTelephone = "format incorrecte. Doit etre 123 123-1234";
+
+echo print_r($_POST["telephone"]);
+
 
 ?>
 
 <?php
-  if(strlen($nom) == mb_strlen($nom)){
-    echo $char = false;
-      }else{
-        $errorNom = " ";
-      }
+
+//code for nom and prenom
+if(strlen($nom) == mb_strlen($nom)){
+  $char = false;
+}else {
+  $errorChar = " ";
+}
+
+if (strlen($nom) <= 2 ){
+  $length = false;
+}else{
+  $errorLength = " ";
+}
+
+
+//code for telephone number
+for ($i = 0; $i <= 11 ; $i++){
+    $c = $telephone[$i];
+    if($c >= "0" && $c <= "9"){
+      $tel = true;
+      $errorTelephone = " ";
+    }
+
+    if($telephone[3] != " "){
+      $tel = false;
+    }else{
+      $errorTelephone = " ";
+    }
+
+    if($telephone[7] != "-"){
+      $tel = false;
+    }else{
+      $errorTelephone = " ";
+    }
+  
+//code for naissance 
+
+if(strlen($naissance) =! 9){
+  $naiss = false;
+}else{
+  $errorNaissance = " ";
+}
+
+$annee = substr($naissance,0,4);
+//insert code for appropriate year
+
+$mois = substr($birth,5,2);
+//insert code for appropriate month
+
+$jour = substr($jour,8,2);
+//insert code for appropriate day
+
 
 ?>
 
@@ -46,23 +96,24 @@ $errorTelephone = "format incorrecte. Doit etre 123-123-1234";
     <br>
     <label>Nom :</label>
     
-    <input type="text" name="nom" value=" "><span>&nbsp;<?= $errorNom ?></span>
+    <input type="text" name="nom" value=" ">
+    <span>&nbsp;<?= $errorChar."<br>".$errorLength ?></span>
     
     
     <br>
     <label>Prenom :</label>
     <input type="text" name="prenom" value="">
-    <span>&nbsp;<?= $errorNom ?></span>
+    <span>&nbsp;<?=  $errorChar."<br>".$errorLength ?></span>
 
     <br>
     <label>Date de naissance :</label>
     <input type="text" name="naissance" value="aaaa-mm-jj">
-    <span>&nbsp;<?= $errorNom ?></span>
+    <span>&nbsp;<?= $errorChar."<br>".$errorLength ?></span>
 
     <br>
     <label>Téléphone :</label>
-    <input type="tel" name="telephone" value="123-123-1234">
-    <span>&nbsp;<?= $errorNom ?></span>
+    <input type="tel" name="telephone" value="123 -1234">
+    <span>&nbsp;<?= $errorTelephone."<br>".$errorLength?></span>
 
     <br>
     <input type="submit" value="Valider"> 
