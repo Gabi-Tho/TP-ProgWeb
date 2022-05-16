e70946f9f52b79a74a875e4519935d349fbff416
Gabi-Tho <102246449+Gabi-Tho@users.noreply.github.com>
2022-05-16 15:50:01 -0400

    elimate error messages on initialization

--- Ajout-clients.php
+++ Ajout-clients.php
@@ -1,6 +1,6 @@
 <?php
 
-echo "<pre>".print_r($_POST, true)."</pre>"; // DEBUG
+// echo "<pre>".print_r($_POST, true)."</pre>"; // tool to help me check 
 
 // ajouter le code de gestion de ce formulaire
 
@@ -10,11 +10,12 @@ $naissance     = $_POST["naissance"] ?? "";
 $telephone     = $_POST["telephone"] ?? "";
 $messageAjout = "Ajout effectué dans la base de données";
 $errorChar = "";
-$erreurDateFormat = " ";
+$erreurDateFormat = "";
 $errorLength = "";
 $errorNaissance = "";
 $errorTelephone = ""; //format incorrecte. Doit etre 123 123-1234
-$errorTelephone1 = "";
+$erreurDateFormat = "";
+
 
 
 ?>
@@ -22,97 +23,105 @@ $errorTelephone1 = "";
 <?php
 
 //code for nom and prenom is correct
-if(strlen($nom) == mb_strlen($nom)){
-  $errorChar = "au moins quelques characteres speciale";
-}else {
-  $errorChar = " ";
-}
 
-if (mb_strlen($nom) <= 2 ){
-  $errorLength = "au moins quelques characteres speciale";
-}else{
-  $errorLength = " ";
-}
+if(count($_POST) > 0){
 
-
-
-$naissance = str_replace("-", " ", $naissance);
-
-date_default_timezone_set("America/Toronto");
-
-echo $annee = substr($naissance,0,4)." ";
-echo $mois = substr($naissance,5,2)." ";
-echo $jour = substr($naissance,8,2)." ";
-
-$annee = intval($annee);
-$mois = intval($mois);
-$jour = intval($mois);
-
-if (checkdate($mois,$jour,$annee)){
-  $erreurDateFormat = " ";
-}else{
-  $erreurDateFormat = "doit avoir le bon format";
-}
-
-echo $anneePresent = date("Y")." ";  
-echo $moisPresent = date("m")." "; 
-echo $jourPresent = date("d");
-
-
-if(substr($naissance,0,4) == 2004 && $mois == $moisPresent && $jour == $jourPresent){
-  $errorNaissance = " ";
-}elseif(substr($naissance,0,4) == 2004 && $mois <= $moisPresent && $jour <= $jourPresent){
-  $errorNaissance = " ";
-}elseif(substr($naissance,0,4) < 2004){
-  $errorNaissance = " ";
-}elseif(substr($naissance,0,4) > 2004){
-  $errorNaissance = "doit etre majeur";
-}
-
+  if(strlen($nom) == mb_strlen($nom)){
+    $errorChar = "au moins quelques characteres speciale";
+  }else {
+    $errorChar = " ";
+  }
   
-//code for telephone
+  if (mb_strlen($nom) <= 2 ){
+    $errorLength = "au moins quelques characteres speciale";
+  }else{
+    $errorLength = " ";
+  }
+  
+  
+  
+  $naissance = str_replace("-", " ", $naissance);
+  
+  date_default_timezone_set("America/Toronto");
+  
+  echo $annee = substr($naissance,0,4)." ";
+  echo $mois = substr($naissance,5,2)." ";
+  echo $jour = substr($naissance,8,2)." ";
+  
+  $annee = intval($annee);
+  $mois = intval($mois);
+  $jour = intval($mois);
+  
+  
+  if (checkdate($mois,$jour,$annee)){
+    $erreurDateFormat = "";
+  }elseif(checkdate($mois,$jour,$annee) == false){
+    $erreurDateFormat = "wrong format";
+  }
+  
+  echo $anneePresent = date("Y")." ";  
+  echo $moisPresent = date("m")." "; 
+  echo $jourPresent = date("d");
+  
+  
+  if(substr($naissance,0,4) == 2004 && $mois == $moisPresent && $jour == $jourPresent){
+    $errorNaissance = " ";
+  }elseif(substr($naissance,0,4) == 2004 && $mois <= $moisPresent && $jour <= $jourPresent){
+    $errorNaissance = " ";
+  }elseif(substr($naissance,0,4) < 2004){
+    $errorNaissance = " ";
+  }elseif(substr($naissance,0,4) > 2004){
+    $errorNaissance = "doit etre majeur";
+  }
+  
+    
+  //code for telephone
+  if (mb_strlen($telephone) == 12){
+    $errorTelephone = " "; 
+  }else{
+    $errorTelephone = "mauvais format";
+  }
+  
+  //check proper spacing
+  if (substr($telephone,3,1) == " "){
+    $errorTelephone1 = " ";
+  }else{
+    $errorTelephone1 = "mauvais format";
+  }
+  //check proper "-"
+  if (substr($telephone,7,1) == "-"){
+    $errorTelephone2 = " ";
+  }else{
+    $errorTelephone2 = "mauvais format";
+  }
+  
+  // check proper number sequence
+  
+  if (is_numeric(substr($telephone,0,3))){
+    $errorTelephone = " ";
+  }else{
+    $errorTelephone = "mauvais format";
+  }
+  if (is_numeric(substr($telephone,4,3))){
+    $errorTelephone = " ";
+  }else{
+    $errorTelephone = "mauvais format";
+  }
+  if (is_numeric(substr($telephone,4,3))){
+    $errorTelephone = " ";
+  }else{
+    $errorTelephone = "mauvais format";
+  }
+  if (is_numeric(substr($telephone,8,4))){
+    $errorTelephone = " ";
+  }else{
+    $errorTelephone = "mauvais format";
+  }
+  
 
-if (mb_strlen($telephone) == 12){
-  $errorTelephone = " "; 
-}else{
-  $errorTelephone = "mauvais format";
 }
 
-//check proper spacing
-if (substr($telephone,3,1) == " "){
-  $errorTelephone1 = " ";
-}else{
-  $errorTelephone1 = "mauvais format";
-}
-//check proper "-"
-if (substr($telephone,7,1) == "-"){
-  $errorTelephone2 = " ";
-}else{
-  $errorTelephone2 = "mauvais format";
-}
 
-// check proper number sequence
-
-if (is_numeric(substr($telephone,0,3))){
-  $errorTelephone3 = " ";
-}else{
-  $errorTelephone3 = "mauvais format";
-}
-if (is_numeric(substr($telephone,4,3))){
-  $errorTelephone4 = " ";
-}else{
-  $errorTelephone4 = "mauvais format";
-}
-if (is_numeric(substr($telephone,4,3))){
-  $errorTelephone5 = " ";
-}else{
-  $errorTelephone5 = "mauvais format";
-}
-if (is_numeric(substr($telephone,8,4))){
-  $errorTelephone6 = " ";
-}else{
-  $errorTelephone6 = "mauvais format";
-}
 
 ?>
 
@@ -132,13 +141,13 @@ if (is_numeric(substr($telephone,8,4))){
 <body>
   <h2>Ajout d'une client</h2>
 
-  <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post">
+  <form action="<?= $_SERVER["PHP_SELF"] ?>" method="post"  >
     
     <br>
     <label>Nom :</label>
     
-    <input type="text" name="nom" value=" ">
-    <span>&nbsp;<?= $errorChar."<br>".$errorLength ?></span>
+    <input type="text" name="nom" value="">
+    <span>&nbsp;<?= $errorLength ?></span>
     
     
     <br>
@@ -154,7 +163,7 @@ if (is_numeric(substr($telephone,8,4))){
     <br>
     <label>Téléphone :</label>
     <input type="tel" name="telephone" value="" placeholder="123 -1234">
-    <span>&nbsp;<?= $errorTelephone."<br>".$errorTelephone1."<br>".$errorTelephone2."<br>".$errorTelephone3."<br>".$errorTelephone4?></span>
+    <span>&nbsp;<?= $errorTelephone?></span>
 
     <br>
     <input type="submit" value="Valider"> 
